171451885f3f5d80871fa88c73efdcfa8cf77074
Doidae <andyarredondo1224@gmail.com>
2025-02-19 13:25:11 -0500

    Styled projects and added sticky navbar

--- script.js
+++ script.js
@@ -28,9 +28,18 @@ document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
 
         if (targetElement) {
             window.scrollTo({
-                top: targetElement.offsetTop - 50,
+                top: targetElement.offsetTop - 500,
                 behavior: "smooth"
             });
         }
     });
 });
+
+window.addEventListener("scroll", function () {
+    let navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("shrink");
+    } else {
+      navbar.classList.remove("shrink");
+    }
+  });
